4e19a9a64a933426df49ff23728979c4bc6a5684
James Park <jamespa@unity3d.com>
2022-09-19 11:46:58 +0200

    version that renders mesh both in game view and scene view

--- Assets/Scripts/RenderMeshInPlayMode.cs
+++ Assets/Scripts/RenderMeshInPlayMode.cs
@@ -5,8 +5,7 @@ public class RenderMeshInPlayMode : MonoBehaviour
     public Material material;
     private Mesh mesh;
 
-    // Start is called before the first frame update
-    void Start()
+    void CreateMesh()
     {
         // Unity is left-handed Y is up, +ve Z is forward. Winding is Clockwise.
         mesh = new Mesh();
@@ -30,10 +29,30 @@ public class RenderMeshInPlayMode : MonoBehaviour
         mesh.RecalculateTangents();
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMesh();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        int layer = 0;
+        Camera camera = null;
+        int submeshIndex = 0;
+        MaterialPropertyBlock matPropBlock = null;
         bool castShadows = false;
-        Graphics.DrawMesh(mesh, Vector3.zero, Quaternion.identity, material, 0, null, 0, null, castShadows);
+
+        Graphics.DrawMesh(mesh, Vector3.zero, Quaternion.identity, material, layer, camera, submeshIndex, matPropBlock, castShadows);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (mesh == null)
+            CreateMesh();
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawMesh(mesh);
     }
 }
